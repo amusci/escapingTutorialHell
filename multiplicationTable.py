601719057d4d60902e656ebd851e9d14b4d45f1c
a <antoniomusciano@gmail.com>
2023-11-20 18:11:24 -0500

    input gang

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -1,4 +1,7 @@
-for y in range(1,11): # columns
-    for x in range(1,11): # row
-        print(x * y, end= " ")
+c = int(input('how many columns: '))
+r = int(input('how many rows: '))
+
+for y in range(1, c + 1):  # columns
+    for x in range(1, r + 1):  # row
+        print(x * y, end=" ")
     print('\n\n')
