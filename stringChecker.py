5b27be253cd25b51a9bea9d084efa10dfce37022
a <antoniomusciano@gmail.com>
2023-11-20 17:35:39 -0500

    dodat

---
+++ stringChecker.py
@@ -0,0 +1,8 @@
+
+
+str_x = input('Enter the string you would like to count over:')
+check = input('Enter the string you would like to check over the string: ')
+
+amount = str_x.count(check)
+
+print(amount)
