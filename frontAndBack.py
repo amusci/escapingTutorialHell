cddd9a529f5659e9ff2fd120fe2328efb3e5498c
a <antoniomusciano@gmail.com>
2023-11-20 16:25:05 -0500

    dub

--- frontAndBack.py
+++ frontAndBack.py
@@ -1,4 +1,15 @@
-numbers_x = [10, 20, 30, 40, 10]
+# numbers_x = [10, 20, 30, 40, 10]
+
+
+numbers_x = []
+
+inp = int(input('How big do you want this list to be?: '))
+
+for i in range(0, inp):
+    x = int(input(f'What should be number in the {i} index?: '))
+    numbers_x.append(x)
+
+print(numbers_x)
 
 if numbers_x[0] == numbers_x[-1]:
     print('yeah')
