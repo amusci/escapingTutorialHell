8dd8b86938ed21c1d3aed6360439ef502d691396
a <antoniomusciano@gmail.com>
2023-11-21 19:44:36 -0500

    Update wordGame.py

--- semi real/wordGame.py
+++ semi real/wordGame.py
@@ -4,21 +4,19 @@ import random
 wordList = ['yup', 'sup', 'one', 'two', 'three', 'population', 'bomb']
 count = 0
 
+
 def getRandomWord(wordList):
-    # This function returns a random string from the passed list of  strings.
+    # This function returns a random string from the passed list of strings.
     wordIndex = random.randint(0, len(wordList) - 1)
+    # print(wordIndex)
     return wordList[wordIndex]
+
+def showBoard(missed,correct,secret):
+
+    blanks = '_' * len(secret)
+
+
 
 
 if __name__ == "__main__":
     getRandomWord(wordList)
-
-
-
-
-
-
-
-
-
-
