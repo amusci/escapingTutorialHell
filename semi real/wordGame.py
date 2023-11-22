86d3d1651a8332e7b68d968b8b959846240d503c
a <antoniomusciano@gmail.com>
2023-11-21 19:37:21 -0500

    idk maybe nfm wordle eventually?
    im braindead

---
+++ semi real/wordGame.py
@@ -0,0 +1,12 @@
+# word guessing game ig
+import random
+
+words = ['yup', 'sup', 'one', 'two', 'three', 'population', 'bomb']
+count = 0
+def welcomeHoe():
+
+
+if __name__ == "__main__":
+    print('WELCOME TO DA GUESSING GAME HOE')
+    inp = input('Guess a letter: ')
+
