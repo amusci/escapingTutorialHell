b6454e3c488c8b5423095ebf5488bfd834baebcd
a <antoniomusciano@gmail.com>
2023-12-05 19:30:01 -0500

    huh

---
+++ c++/addingTwoInt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int num1;
+    int num2;
+    int ans;
+
+    num1 = 1;
+    num2 = 2;
+    ans = num1 + num2;
+
+    std::cout << num1 << "+" << num2 << "is " << ans << std::endl;
+
+}
