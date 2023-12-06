908f12043f58ea7f42c2499c8e7629052e4c0e36
a <antoniomusciano@gmail.com>
2023-12-05 19:55:43 -0500

    muplication
    muplication

---
+++ c++/muplication.cpp
@@ -0,0 +1,17 @@
+//mulplication
+
+#include <iostream>
+
+int main() {
+    double num1 = 5;
+    double num2 = 6;
+    double ans;
+
+    ans = num1 * num2;
+
+    std::cout << ans << std::endl;
+
+
+
+    return 0;
+}
