a7bce8162da1f92f4b29b7752da9751b61a8bed2
a <antoniomusciano@gmail.com>
2023-12-07 15:52:27 -0500

    return

---
+++ c++/minutesToSeconds.cpp
@@ -0,0 +1,21 @@
+//minutes to seconds, return da answer doe
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+// return_statement2.cpp
+#include <stdio.h>
+
+int convert( int minutes )
+{
+    int s = minutes * 60;
+    return s;
+}
+
+int main()
+{
+    std::cout << convert(5) << std::endl;
+}
