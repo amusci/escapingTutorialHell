728846179c94b76514c36379815eb5e4a7218f0e
a <antoniomusciano@gmail.com>
2023-12-08 23:20:26 -0500

    Update addingTwoInt.cpp

--- c++/addingTwoInt.cpp
+++ c++/addingTwoInt.cpp
@@ -1,14 +1,10 @@
+
+
 #include <iostream>
 
 int main() {
-    int num1;
-    int num2;
-    int ans;
 
-    num1 = 1;
-    num2 = 2;
-    ans = num1 + num2;
 
-    std::cout << num1 << "+" << num2 << "is " << ans << std::endl;
+
 
 }
