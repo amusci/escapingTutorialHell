cfceb94b314fbda1f7e9c48c58dd2c78c3463294
a <antoniomusciano@gmail.com>
2024-01-08 15:34:50 -0500

    four int equal

---
+++ java/fourEqual.java
@@ -0,0 +1,13 @@
+public static boolean four (int a, int b, int c, int d) {
+
+    if ( a != b && a != c && a != d) {
+        System.out.println("false");
+        return false;
+
+
+    } else {
+        System.out.println("true");
+        return true;
+    }
+
+}
