5c8845919a1da5775c7c75899f1c57db322bfec8
a <antoniomusciano@gmail.com>
2024-01-09 15:24:01 -0500

    1st fizzbuzz

---
+++ semi real/fizzBuzz.java
@@ -0,0 +1,22 @@
+private static void fizzBuzz(int number) {
+
+    if (number % 3 != 0 && number % 5 != 0) {
+
+        System.out.println(number);
+
+    }
+    else if (number % 3 == 0 && number % 5 == 0) {
+
+        System.out.println("FizzBuzz");
+
+    }
+    else if (number % 3 == 0) {
+
+        System.out.println("Fizz");
+    }
+    else if (number % 5 == 0) {
+
+        System.out.println("Buzz");
+    }
+
+}
