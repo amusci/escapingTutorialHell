ea40cc68263839cc44a324c0c5fda31c8ee220d0
a <antoniomusciano@gmail.com>
2024-02-12 14:14:26 -0500

    brute force

--- pyt/containsDuplicates.py
+++ pyt/containsDuplicates.py
@@ -1,12 +1,13 @@
 def contains_duplicateSet(nums):
-    seen = set() # create a set (NO DUPLICATES)
-    for num in nums: # iterate through the list of numbers
+    seen = set()  # create a set (NO DUPLICATES)
+    for num in nums:  # iterate through the list of numbers
         print(seen)
-        if num in seen: # if the num is in seen
-            return True # return true
-        seen.add(num) # ties to the for loop, adding numbers to the set
+        if num in seen:  # if the num is in seen
+            return True  # return true
+        seen.add(num)  # ties to the for loop, adding numbers to the set
 
-    return False # return false if no duplicates
+    return False  # return false if no duplicates
+
 
 def contains_duplicatesSort(nums):
     nums.sort()  # sort the list
@@ -14,3 +15,13 @@ def contains_duplicatesSort(nums):
         if nums[i] == nums[i - 1]:  # if index 2 == index 1
             return True  # return true if dups
     return False  # return false if no dups
+
+
+def contains_duplicatesBF(nums):
+    n = len(nums)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            print(nums[i], nums[j])
+            if nums[i] == nums[j]:
+                return True
+    return False
