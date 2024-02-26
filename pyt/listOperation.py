c6e156b31238701a0a015792382919acf042af3f
a <antoniomusciano@gmail.com>
2024-02-26 15:55:11 -0500

    overthink

---
+++ pyt/listOperation.py
@@ -0,0 +1,21 @@
+"""
+
+Create a function that takes three parameters where:
+
+    x is the start of the range (inclusive).
+    y is the end of the range (inclusive).
+    n is the divisor to be checked against.
+
+Return an ordered list with numbers in the range that are divisible by the third parameter n.
+
+Return an empty list if there are no numbers that are divisible by n.
+
+"""
+
+
+def list_operation(x, y, n):
+    ans = []
+    for i in range(x,y + 1):
+        if i % n == 0:
+            ans.append(i)
+    return ans
