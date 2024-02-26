18fb487e16053fc2224a62c82475b31c106857e6
a <antoniomusciano@gmail.com>
2024-02-26 15:01:07 -0500

    underthinking solution

--- pyt/showdown.py
+++ pyt/showdown.py
@@ -11,27 +11,5 @@ If both are drawn at the same time, return "tie".
 """
 
 def showdown(p1, p2):
-    bet = " "
-    bang = "B", "a", "n", "g", "!"
-
-    p1_count = 0
-    p2_count = 0
-    for i in p1:
-        if i in bet:
-            p1_count += 1
-        elif i in bang:
-            break
-
-    for j in p2:
-        if j in bet:
-            p2_count += 1
-        elif j in bang:
-            break
-
-    print(p1_count, p2_count)
-    if p1_count < p2_count:
-        return "p1"
-    elif p2_count < p1_count:
-        return "p2"
-    else:
-        return "tie"
+    shot1, shot2 = p1.index('B'), p2.index('B')
+    return 'p1' if shot1 < shot2 else 'p2' if shot2 < shot1 else 'tie'
