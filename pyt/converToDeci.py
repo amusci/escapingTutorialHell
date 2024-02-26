f9477c2fd4eca77d0fd5dcece00f45d82e980310
a <antoniomusciano@gmail.com>
2024-02-26 16:33:40 -0500

    python answer

--- pyt/converToDeci.py
+++ pyt/converToDeci.py
@@ -4,7 +4,4 @@ Create a function to convert a list of percentages to their decimal equivalents.
 
 """
 def convert_to_decimal(perc):
-    ans = []
-    for i in perc:
-        ans.append(float(i.strip('%'))/100)
-    return ans
+    return [float(i.strip('%')) / 100 for i in perc]
