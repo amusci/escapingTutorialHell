51fe64fe83bfbbda42a6998154d3b8d5e0054198
a <antoniomusciano@gmail.com>
2024-02-26 13:53:00 -0500

    better solution now that i didnt overthink

--- pyt/addingStrings.py
+++ pyt/addingStrings.py
@@ -6,8 +6,8 @@ Create a function that takes two number strings and returns their sum as a strin
 
 
 def add(n1, n2):
-    if n1 is None or n1 is "" or n2 is None or n2 is "":
-        return "Invalid Operation"
-    else:
+    if n1 and n2:
         wee = int(n1) + int(n2)
         return '{}'.format(wee)
+    else:
+        return "Invalid Operation"
