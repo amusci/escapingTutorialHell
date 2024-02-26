eb2fbc7c5a561157577a868290ee3c3d3aec2877
a <antoniomusciano@gmail.com>
2024-02-26 16:49:09 -0500

    noob method

---
+++ pyt/addIndexes.py
@@ -0,0 +1,13 @@
+"""
+
+Given a list of numbers, create a function which returns the list but with each element's index in the list added to itself.
+
+This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
+
+"""
+
+def add_indexes(lst):
+    yeah = []
+    for i,j in enumerate(lst):
+        yeah.append(i + j)
+    return yeah
