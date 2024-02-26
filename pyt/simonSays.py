908210c7373d2cd5f68dfb90210f115dcb3e1b07
a <antoniomusciano@gmail.com>
2024-02-26 15:49:22 -0500

    underthink method

--- pyt/simonSays.py
+++ pyt/simonSays.py
@@ -8,13 +8,7 @@ In other words, determine if the second list is the first list shifted to the ri
 
 
 def simon_says(lst1, lst2):
-    count = 0
-    n = len(lst1)
-    for i in range(n - 1):
-        print(lst1[i], lst2[i + 1])
-        if lst1[i] == lst2[i + 1]:
-            count += 1
-    if count == n - 1:
-        return True
-    else:
-        return False
+	if lst1[:-1] == lst2[1:]:
+		return True
+	else:
+		return False
