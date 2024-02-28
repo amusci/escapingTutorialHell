a3f75e176de69f98157bacfb0bf9452b3703269b
a <antoniomusciano@gmail.com>
2024-02-27 20:17:31 -0500

    why is python like this

--- pyt/isAnagram.py
+++ pyt/isAnagram.py
@@ -5,15 +5,4 @@ Create a function that takes two strings and returns either True or False depend
 """
 
 def is_anagram(s1, s2):
-    clean_s1 = s1.lower()
-    clean_s2 = s2.lower()
-
-    s_1 = list(clean_s1)
-    s_2 = list(clean_s2)
-    print(s_1,s_2)
-    s_1.sort()
-    s_2.sort()
-    if s_1 == s_2:
-        return True
-
-    return False
+    return sorted(s1.upper())==sorted(s2.upper())
