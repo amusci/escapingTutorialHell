72554b03068673e7dcb2b2e21437a13f484e2d2e
a <antoniomusciano@gmail.com>
2024-03-01 15:45:37 -0500

    woohoo python

--- pyt/setify.py
+++ pyt/setify.py
@@ -6,12 +6,4 @@ Create a function that sorts a list and removes all duplicate items from it.
 
 
 def setify(lst):
-    lst.sort()
-    ans = []
-    seen = set()
-    for i in lst:
-        if i not in seen:
-            ans.append(i)
-            seen.add(i)
-
-    return ans
+    return sorted(set(lst))
