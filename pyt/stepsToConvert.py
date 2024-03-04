a444a180ed0b5d27bd64cacd00ae03432418159c
a <antoniomusciano@gmail.com>
2024-03-04 16:31:48 -0500

    python woohoo

--- pyt/stepsToConvert.py
+++ pyt/stepsToConvert.py
@@ -9,16 +9,6 @@ A step consists of changing one character from lower to upper case, or vice vers
 """
 
 def steps_to_convert(txt):
-    n = len(txt)
-    upper_list = []
-    lower_list = []
-    for i in txt:
-        if i.isupper():
-            upper_list.append(i)
-        else:
-            lower_list.append(i)
-
-    if len(upper_list) > len(lower_list):
-        return n - len(upper_list)
-    else:
-        return n - len(lower_list)
+    lower = len([i for i in txt if i.islower()])
+    upper = len([i for i in txt if i.isupper()])
+    return min(lower,upper)
