90700f65d341d31af48c5999f8c5af5b21f8424a
a <antoniomusciano@gmail.com>
2024-03-04 15:58:07 -0500

    forgot abt count lol

--- pyt/unique.py
+++ pyt/unique.py
@@ -6,12 +6,7 @@ Create a function that takes a list of numbers and returns the number that's uni
 
 
 def unique(lst):
-    counts = {}
-    for num in lst:
-        counts[num] = counts.get(num,0) + 1
-
-    for num,count in counts.items():
-        if count == 1:
-            return num
-
-    return None
+    def unique(lst):
+        for num in lst:
+            if lst.count(num) == 1:
+                return num
