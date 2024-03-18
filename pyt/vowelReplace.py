605a226c63c7328a1488aed3bf4ce9721593aaeb
a <antoniomusciano@gmail.com>
2024-03-18 13:35:06 -0400

    way better solution

--- pyt/vowelReplace.py
+++ pyt/vowelReplace.py
@@ -8,10 +8,7 @@ Write a function that replaces all vowels in a string with a specified vowel.
 def vow_replace(word, vowel):
 
     vowels = 'aeiou'
-    ans = ''
     for char in word:
         if char in vowels:
-            ans += vowel
-        else:
-            ans += char
-    return ans
+            word = word.replace(char, vowel)
+    return word
