ee50ee4b4667a68cba0e9fb3c8b40578f97e110f
a <antoniomusciano@gmail.com>
2024-03-19 16:43:44 -0400

    space dat

---
+++ pyt/correctSpacing.py
@@ -0,0 +1,13 @@
+"""
+
+Additional spaces have been added to a sentence. Return the correct sentence by removing them.
+
+All words should be separated by one space, and there should be no spaces at the beginning or end of the sentence.
+
+"""
+
+
+
+
+def correct_spacing(sentence):
+    return ' '.join(sentence.split())
