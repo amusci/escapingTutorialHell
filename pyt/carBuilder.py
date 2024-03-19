bb844b9b44b244079661c82e1c143bd7203b3978
a <antoniomusciano@gmail.com>
2024-03-19 16:26:29 -0400

    so mad i didnt think of this sooner

--- pyt/carBuilder.py
+++ pyt/carBuilder.py
@@ -10,13 +10,4 @@ Given the total number of wheels, car bodies and figures available, how many com
 
 
 def cars(wheels, bodies, figures):
-    ans = 0
-    if wheels < 4 or bodies < 1 or figures < 2:
-        return 0
-    else:
-        while wheels >= 4 and bodies >= 1 and figures >= 2:
-            ans += 1
-            wheels -= 4
-            bodies -= 1
-            figures -= 2
-        return ans
+    return min(wheels // 4, bodies, figures // 2)
