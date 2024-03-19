7867622c75ae6cdd45f013abff59612894dec3f1
a <antoniomusciano@gmail.com>
2024-03-19 16:05:26 -0400

    python solution

--- pyt/removeEnemies.py
+++ pyt/removeEnemies.py
@@ -6,8 +6,4 @@ Remove enemies from the list of people, even if the enemy shows up twice.
 
 
 def remove_enemies(names, enemies):
-    ans = []
-    for enemy in names:
-        if enemy not in enemies:
-            ans.append(enemy)
-    return ans
+    return [i for i in names if i not in enemies]
