a85f3e978579711f4ac1056328eb0f8ac65cc1e8
a <antoniomusciano@gmail.com>
2024-03-26 19:45:25 -0400

    python solution omg

--- pyt/count_all.py
+++ pyt/count_all.py
@@ -3,11 +3,6 @@
 Return the result in a dictionary.'''
 
 
-def count_all(str):
-    ans = {"LETTERS": 0, "DIGITS": 0}
-    for i in str:
-        if i.isalpha():
-            ans["LETTERS"] += 1
-        elif i.isdigit():
-            ans["DIGITS"] += 1
-    return ans
+def count_all(txt):
+    return {"LETTERS": sum (c.isalpha() for c in txt),
+    "DIGITS": sum(c.isdigit() for c in txt)}
