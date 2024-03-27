b890c0eb400a8e5b2320585cdc08669e82c0c3ce
a <antoniomusciano@gmail.com>
2024-03-26 23:52:05 -0400

    so i learned that isdigit() isalpha() accounts for the whole string too

--- pyt/alphanumeric_restrictions.py
+++ pyt/alphanumeric_restrictions.py
@@ -9,16 +9,4 @@ If a string has both numbers and letters, or contains characters which don't fit
 '''
 
 def alphanumeric_restriction(s):
-    if not s:
-        return False
-
-    for char in s:
-        if not char.isalnum():
-            return False
-
-    has_letters = any(char.isalpha() for char in s)
-    has_numbers = any(char.isdigit() for char in s)
-
-    if has_numbers == True and has_letters == True:
-        return False
-    return True
+    return s.isdigit() or s.isalpha()
