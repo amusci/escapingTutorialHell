53f246ebdb018095978386dcb27243ad29483fc0
a <antoniomusciano@gmail.com>
2024-03-27 00:24:08 -0400

    Update convert_binary.py

--- pyt/convert_binary.py
+++ pyt/convert_binary.py
@@ -6,11 +6,9 @@ Write a function that transforms all letters from [a, m] to 0 and letters from [
 def convert_binary(string):
     ans = ''
     a_to_m = 'abcdefghijklm'
-    n_to_z = 'nopqrstuvwxyz'
-
     for char in string.lower():
         if char in a_to_m:
             ans += '0'
-        elif char in n_to_z:
+        else:
             ans += '1'
     return ans
