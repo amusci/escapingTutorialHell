03296902df2c06d497fe872f99125cb462de7d0c
a <antoniomusciano@gmail.com>
2024-03-28 00:49:02 -0400

    ugly dict

--- pyt/spaceWeights.py
+++ pyt/spaceWeights.py
@@ -24,7 +24,13 @@ Given a weight weighed on planet_a, return the converted value for planet_b roun
 
 
 def space_weights(planet_a, weight, planet_b):
-    planets = {"Mercury": 3.7, "Venus": 8.87, "Earth": 9.81, "Mars": 3.711, "Jupiter": 24.79, "Saturn": 10.44,
-               "Uranus": 8.69, "Neptune": 11.15}
+    planets = {"Mercury": 3.7,
+               "Venus": 8.87,
+               "Earth": 9.81,
+               "Mars": 3.711,
+               "Jupiter": 24.79,
+               "Saturn": 10.44,
+               "Uranus": 8.69,
+               "Neptune": 11.15}
 
     return round(weight / planets[planet_a] * planets[planet_b], 2)
