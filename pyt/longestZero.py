1d808d7c9bc9283beb12b57eae3b4ac649f9750f
a <antoniomusciano@gmail.com>
2024-04-01 19:31:55 -0400

    good solution

--- pyt/longestZero.py
+++ pyt/longestZero.py
@@ -7,19 +7,13 @@ Write a function that returns the longest sequence of consecutive zeroes in a bi
 
 
 def longest_zero(s):
-    lenny = len(s)
-    ans = ''
-    max_ans = ''
+    longest = 0
+    answer = longest
 
-    for i in range(lenny):
-        if s[i] == '0':
-            ans += s[i]
+    for i in s:
+        if i == "0":
+            longest += 1
         else:
-            if len(ans) > len(max_ans):
-                max_ans = ans
-            ans = ''
-
-    if len(ans) > len(max_ans):
-        max_ans = ans
-
-    return max_ans
+            answer = max(longest, answer)
+            longest = 0
+    return "0" * max(longest,answer)
