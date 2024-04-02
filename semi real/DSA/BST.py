231201b728ab0b5bbffe1bf44be5580815c9da50
a <antoniomusciano@gmail.com>
2024-04-02 15:43:58 -0400

    inorder traversal

--- semi real/DSA/BST.py
+++ semi real/DSA/BST.py
@@ -46,18 +46,28 @@ class TreeNode:
         self.value = value
 
     def insert(self, value):
-        if value < self.value: # if the value you want to insert is less than the value of the current node (we go left)
-            if self.left is None: # if theres no value in the left node
-                self.left = TreeNode(value) # add the value to the empty left node
+        if value < self.value:  # if the value you want to insert is less than the value of the current node (we go left)
+            if self.left is None:  # if theres no value in the left node
+                self.left = TreeNode(value)  # add the value to the empty left node
             else:
-                self.left.insert(value) # if the node already has a value, recursively call insert on the left node
+                self.left.insert(value)  # if the node already has a value, recursively call insert on the left node
 
-        else: # if value > the value of the node, it must be placed on the right
-            if self.right is None: # if the value of the is none
-                self.right = TreeNode(value) # add the value to the empty right node
+        else:  # if value > the value of the node, it must be placed on the right
+            if self.right is None:  # if the value of the is none
+                self.right = TreeNode(value)  # add the value to the empty right node
 
             else:
-                self.right.insert(value) # if the node already has a value, recursively call insert on the right node
+                self.right.insert(value)  # if the node already has a value, recursively call insert on the right node
+
+    def inorder_traversal(self):
+        if self.left:  # if left node exists
+            self.left.inorder_traversal()  # call inorder on that node, repeat until you reach most left node
+        print(self.value)  # prints value of parent node
+        if self.right:  # if right node exists
+            self.right.inorder_traversal()  # recursively call
+
+            #  note that we do NOT do another print, as we will print the left before the right, allowing it to be in
+            #  the correct order
 
 
 tree = TreeNode(10)
@@ -69,6 +79,7 @@ tree.insert(55)
 tree.insert(16)
 tree.insert(77)
 
-print(tree.right.right.right.left.value) # this will be 16
+# print(tree.right.right.right.left.value)  # this will be 16
+tree.inorder_traversal()  # this will be 4, 8, 10, 11, 12, 16, 55, 77
 
 # https://www.cs.usfca.edu/~galles/visualization/BST.html
