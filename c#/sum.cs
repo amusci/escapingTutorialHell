deb535deecdc126a290f601162639a84e2adc049
a <antoniomusciano@gmail.com>
2024-04-04 17:06:18 -0400

    c# arc

---
+++ c#/sum.cs
@@ -0,0 +1,11 @@
+    public static int Sum(int a, int b)
+
+    /* Create a function that takes two numbers as arguments and returns their sum. */
+    {
+        int ans;
+
+        ans = a + b;
+
+        return ans;      
+
+    }
