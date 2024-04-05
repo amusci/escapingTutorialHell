32d91f930ee47c9c79beda1821bb1246eff155d5
a <antoniomusciano@gmail.com>
2024-04-04 23:31:28 -0400

    learned about All

--- c#/sameCase.cs
+++ c#/sameCase.cs
@@ -1,27 +1,17 @@
     public static bool SameCase(string str)
     {
-        char[] ch = str.ToCharArray();
-        int upperCount = 0;
-        int lowerCount = 0;
-        int s = str.Length;
+    public static bool SameCase(string str)
+    {
 
-        for (int i = 0; i < s; i++)
+        char[] ch = str.ToCharArray();
+        if (ch.All(char.IsLower) == true || ch.All(char.IsUpper) == true)
         {
-            if (char.IsUpper(ch[i]))
-            {
-                upperCount ++;
-            }
-            else if (char.IsLower(ch[i]))
-            {
-                lowerCount++;
-            }
-        }
-        if (upperCount != s && lowerCount != s)
-        {
-            return false;
-        }
-        else 
-        {
+
             return true;
+
         }
+
+        return false;
+
+    }
     }
