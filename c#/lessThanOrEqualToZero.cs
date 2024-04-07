f1edf0d7e1773d15874111624109bf9216816088
a <antoniomusciano@gmail.com>
2024-04-07 17:00:45 -0400

    ok this works too

--- c#/lessThanOrEqualToZero.cs
+++ c#/lessThanOrEqualToZero.cs
@@ -2,18 +2,6 @@
     /* Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false. */
     {
 
-      if (a > 0)
-      {
-        return false;
-
-      }
-      else if (a <= 0)
-      {
-
-        return true;
-
-      }
-
-      return false;
+      return a <= 0;
     	
     }
