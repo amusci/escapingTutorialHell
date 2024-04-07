2af65e46884f816743a59aad391f7dc1cfba5823
a <antoniomusciano@gmail.com>
2024-04-07 17:50:20 -0400

    need to be long for exponent

---
+++ c#/exponentCalc.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+    public static long СalculateExponent(long number, long exponent) 
+    /* Create a function that takes a base number and an exponent number and returns the calculation. */
+    {
+      return (long)Math.Pow(number, exponent);
+
+    }
