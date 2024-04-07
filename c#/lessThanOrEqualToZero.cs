04129aee4293e8dc913d1b0ab6b4b6a13b070d39
a <antoniomusciano@gmail.com>
2024-04-07 17:00:17 -0400

    Create lessThanOrEqualToZero.cs

---
+++ c#/lessThanOrEqualToZero.cs
@@ -0,0 +1,19 @@
+    public static bool lessThanOrEqualToZero(double a) 
+    /* Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false. */
+    {
+
+      if (a > 0)
+      {
+        return false;
+
+      }
+      else if (a <= 0)
+      {
+
+        return true;
+
+      }
+
+      return false;
+    	
+    }
