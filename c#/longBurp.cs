3005d91fcb016867a2a30e53f46213de9a933fe0
a <antoniomusciano@gmail.com>
2024-04-07 17:16:23 -0400

    so you cant do "foo" * int bar

---
+++ c#/longBurp.cs
@@ -0,0 +1,16 @@
+	public static string LongBurp(int b) 
+  /* Create a function that returns the string "Burp" with the amount of "r's" determined by the input parameters of the function. */
+	  {
+
+      string bu = "Bu";
+
+      for (int i = 0; i < b; i++) {
+
+        bu += "r";
+
+
+      }
+
+      return bu + "p";
+		
+	  }
