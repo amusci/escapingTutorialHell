a5275102f0dd761eabf1ead34f8434b0d3ed9c09
a <antoniomusciano@gmail.com>
2024-04-07 16:45:36 -0400

    moon

---
+++ c#/areaTriangle.cs
@@ -0,0 +1,10 @@
+    public static int triArea(int b, int h)
+    /* Write a function that takes the base and height of a triangle and return its area. */
+    /* (base * height) / 2 */
+    {
+
+      return (b * h) / 2;
+
+
+    
+    }
