2da3bd93c3315ffa8b03325e5cd56f1dbe8b2081
a <antoniomusciano@gmail.com>
2024-04-07 17:08:10 -0400

    Create powerCalc.cs

---
+++ c#/powerCalc.cs
@@ -0,0 +1,9 @@
+    public static int CircuitPower(int voltage, int current) 
+    /* Create a function that takes voltage and current and returns the calculated power. */
+    {
+
+      return voltage * current;
+
+
+			
+    }
