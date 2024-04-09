5e27f5527f562a251c024b72159b6e6e7b3d1575
a <antoniomusciano@gmail.com>
2024-04-09 16:26:13 -0400

    josie

---
+++ c#/repetition.cs
@@ -0,0 +1,14 @@
+		public static string Repetition(string txt, int n)
+    /*Create a recursive function that takes two parameters and repeats the string n number of times.
+    The first parameter txt is the string to be repeated and the second parameter is the number of times the string is to be repeated.*/
+		{
+      if (n == 1)
+      {
+        return txt;
+
+      }
+
+      return txt + Repetition(txt, n - 1);
+      
+			
+		}
