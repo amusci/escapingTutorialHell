ded611858f5aa4c40aef37d659a2f8db8cd48613
a <antoniomusciano@gmail.com>
2024-04-11 22:06:09 -0400

    Create arrayOfMultiples.cs

---
+++ c#/arrayOfMultiples.cs
@@ -0,0 +1,22 @@
+        public static int[] ArrayOfMultiples(int num, int length)
+         Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length. 
+        
+        {
+
+            int[] ans = new int[length];
+
+
+
+            for (int i = 0; i  length; i++)
+
+            {
+
+                ans[i] = num  (i + 1);;
+
+            }
+
+            return ans;
+
+
+            
+        }
