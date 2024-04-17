c3f2ecf107ba8aa4d7ae8199b3ad056b139e7c2a
Antonio Musciano <antoniomusciano@gmail.com>
2024-04-17 14:08:50 -0400

    papa
    int foo = 1234;
    int bar = 9876;
    $"{foo:n}";  // Output: 1,234.00
    $"{bar:n0}"; // No digits after the decimal point. Output: 9,876

---
+++ c#/currencyFormatting.cs
@@ -0,0 +1,10 @@
+        public static string FormatNum(int num)
+        /* Create a function that takes a number as an argument and returns a string formatted to separate thousands. */
+
+
+        {
+            return $"{num:n0}";
+
+            
+                
+        }
