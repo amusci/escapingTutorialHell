af8632c082caea9db692fd56e1b4b31e46620870
Antonio Musciano <antoniomusciano@gmail.com>
2024-04-17 13:15:28 -0400

    better solution

--- c#/gcd.cs
+++ c#/gcd.cs
@@ -2,30 +2,29 @@
         /* Write a function that returns the greatest common divisor (GCD) of two integers.*/
         {
 
+            while (n1 != 0 && n2 != 0) {
+
+
+                if (n1 > n2) {
+
+                    return gcd(n1, n1-n2);
+
+                } else {
+
+                    return gcd(n2, n2- n1);
+
+                }
+            }
+
             if (n1 == 0) {
 
                 return n2;
 
-            }  
-            else if (n2 == 0) {
+            } else {
 
-                return n1;
-
-            }
-            else if (n1 == n2) {
 
                 return n1;
 
             }
 
-            else if (n1 > n2) {
-
-                return gcd(n1 - n2, n2);
-
-
-
-            }
-            return gcd(n1, n2 - n1);
-
-
-            }
+        }
