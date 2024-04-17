b04c688bcee2f9c5e4cd3dd743cea14156ac6b78
Antonio Musciano <antoniomusciano@gmail.com>
2024-04-17 13:36:25 -0400

    Create tetra.cs

---
+++ c#/tetra.cs
@@ -0,0 +1,11 @@
+        public static int Tetra(int n)
+        {
+            /* A tetrahedron is a pyramid with a triangular base and three sides. A tetrahedral number is a number of items within a tetrahedron.
+
+            Create a function that takes an integer n and returns the nth tetrahedral number. 
+            Tn = (n * (n + 1) * (n + 2)) / 6
+            */
+
+            return (n * (n + 1) * (n + 2)) / 6;
+            
+        }
