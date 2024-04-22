f732f88ffe136abf78d55f4990ae7f82e950ce6f
Antonio Musciano <antoniomusciano@gmail.com>
2024-04-22 12:22:58 -0400

    linqqq

--- c#/findLargest.cs
+++ c#/findLargest.cs
@@ -1,18 +1,9 @@
         public static double[] FindLargest(double[][] values) 
 
-        /* Create a function that takes an array of arrays with numbers. Return a new (single) array with the largest numbers of each.*/
+        /* Create a function that takes an array of arrays with numbers. Return a new (single) array with the largest numbers of each. */
+
         {
 
-            List<double> list = new List<double>();
-
-            foreach (double[] valueArray in values)
-            {
-                double largest = valueArray.Max();
-                list.Add(largest);
-
-
-            }
-
-            return list.ToArray();
-
+            return values.Select(x => x.Max()).ToArray();
+                
         }
