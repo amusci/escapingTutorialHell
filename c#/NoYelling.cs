e15279da8c320ef3d013daa7759e199b1432b8ff
Antonio Musciano <antoniomusciano@gmail.com>
2024-04-24 14:51:57 -0400

    way better solution

--- c#/NoYelling.cs
+++ c#/NoYelling.cs
@@ -4,44 +4,21 @@
         Create a function that transforms sentences ending with multiple question marks ? or exclamation marks ! into a sentence only ending with one without changing punctuation in the middle of the sentences.
         */
         {
-            string result = "";
 
-            string[] splitString = phrase.Split(' ');
-
-            int lengthOfSplitString = splitString.Length;
-            int lengthOfLastSplit = splitString[lengthOfSplitString - 1].Length;
-
-            for (int i = 0; i < lengthOfSplitString - 1; i++)
+            if (phrase.EndsWith('!'))
             {
 
-                result += splitString[i] + " ";
+                return phrase.TrimEnd('!') + '!';
+
+            }
+            else if (phrase.EndsWith('?'))
+            {
+
+                return phrase.TrimEnd('?') + '?';
 
             }
 
-            int count = 0;
-            for (int i = 0; i < lengthOfLastSplit; i++)
-            {
-
-                if (count > 0  && splitString[lengthOfSplitString - 1][i] == '!' ||count > 0 && splitString[lengthOfSplitString - 1][i]== '?')
-                {
+            return phrase;
 
 
-
-                }
-                else if (count == 0  && splitString[lengthOfSplitString - 1][i] == '!' ||count == 0 && splitString[lengthOfSplitString - 1][i] == '?')
-                {
-
-                    count ++;
-                    result += splitString[lengthOfSplitString - 1][i];
-
-                }
-                else 
-                {
-
-                    result += splitString[lengthOfSplitString - 1][i];
-
-                }
-            }
-
-            return result;
         }
