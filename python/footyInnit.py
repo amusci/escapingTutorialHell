ae5340765cc36f1c505bd2e141c90069ef8c544d
Antonio Musciano <71357098+frky1@users.noreply.github.com>
2024-06-04 16:04:21 -0400

    football player class

---
+++ python/footyInnit.py
@@ -0,0 +1,46 @@
+'''
+Create a class that takes the following four arguments for a particular football player:
+
+    name
+    age
+    height
+    weight
+
+Also, create three functions for the class that returns the following strings:
+
+    get_age() returns "name is age age"
+    get_height() returns "name is heightcm"
+    get_weight() returns "name weighs weightkg"
+
+'''
+
+'''
+EXAMPLES:
+
+p1 = player("David Jones", 25, 175, 75)
+
+p1.get_age() ➞ "David Jones is age 25"
+p1.get_height() ➞ "David Jones is 175cm"
+p1.get_weight() ➞ "David Jones weighs 75kg"
+'''
+class player:
+
+    def __init__(self, name, age, height, weight):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+    def get_age(self):
+        return self.name + " is age " + str(self.age)
+    
+    def get_height(self):
+        return self.name + " is " + str(self.height) + "cm"
+    
+    def get_weight(self):
+        return self.name + " weighs " + str(self.weight) + "kg"
+    
+
+
+
+
