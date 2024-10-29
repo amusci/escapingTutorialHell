2471fc10572e9fca7982b24738306731a8a0da18
a <antoniomusciano@gmail.com>
2024-10-29 18:51:46 -0400

    spread

---
+++ js/minMax.js
@@ -0,0 +1,7 @@
+/* Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order. */
+
+function minMax(arr) {
+
+    return [Math.min(...arr), Math.max(...arr)]
+	
+}
