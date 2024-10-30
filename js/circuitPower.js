3b259c298f200e24b910ebb4e80e74eea0225b5e
a <antoniomusciano@gmail.com>
2024-10-29 22:44:47 -0400

    Create circuitPower.js

---
+++ js/circuitPower.js
@@ -0,0 +1,16 @@
+/*
+
+Create a function that takes voltage and current and returns the calculated power.
+
+*/
+
+function circuitPower(voltage, current) {
+
+
+    return voltage * current;
+
+	
+}
+
+
+console.log(circuitPower(230, 10));
